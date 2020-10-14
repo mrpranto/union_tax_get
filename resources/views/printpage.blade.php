c33415a63d9d5a360836132a466915c55b707296
spprince007 <spprince007@gmail.com>
2020-10-14 16:51:14 +0600

    new table add

--- resources/views/printpage.blade.php
+++ resources/views/printpage.blade.php
@@ -10,14 +10,14 @@
             
             <div class="col-md-12">
 
-            <h1 class="text-center mt-3">১০ হরিশংকরপুর ইউনিয়ন পরিষদ</h1>
-            <h2 class="text-center">ডাকঘরঃ হরিশংকরপুর, উপজেলা ও জেলাঃ ঝিনাইদহ। মোবাঃ ০১৭২৪-২৯৯৭৯৯  </h2>
+             <h1 class="text-center mt-3">১০ হরিশংকরপুর ইউনিয়ন পরিষদ</h1>
+             <h2 class="text-center">ডাকঘরঃ হরিশংকরপুর, উপজেলা ও জেলাঃ ঝিনাইদহ। <br>মোবাঃ ০১৭২৪-২৯৯৭৯৯  </h2>
 
 
-<table class="table table-bordered" >
+                <table class="table table-bordered" >
    
     
-        <tbody>
+                <tbody>
                             <tr>
                                 <td>খানা প্রধানের নামঃ</td>
                                 <td>জিমর</td>
@@ -55,9 +55,63 @@
     
     
     
-</table>
+                    </table>
 
-</div>
+                </div>
+                <div class="col-md-12">
+                    <hr class="new2">
+                </div>
+                
+                <div class="col-md-12">
+
+             <h1 class="text-center mt-3">১০ হরিশংকরপুর ইউনিয়ন পরিষদ</h1>
+             <h2 class="text-center">ডাকঘরঃ হরিশংকরপুর, উপজেলা ও জেলাঃ ঝিনাইদহ।<br> মোবাঃ ০১৭২৪-২৯৯৭৯৯  </h2>
+
+
+                <table class="table table-bordered" >
+   
+    
+                <tbody>
+                            <tr>
+                                <td>খানা প্রধানের নামঃ</td>
+                                <td>জিমর</td>
+                                <td>হোল্ডিং নং</td>
+                                <td>12</td>
+                                <td>বিল নং</td>
+                                <td>12</td>
+                                <td>ইস্যুর তারিখ</td>
+                                <td>10-7-20</td>
+                            </tr>
+
+                            <tr>
+                                <td>পিতার নাম</td>
+                                <td>জমির রশিদ</td>
+                                <td>অর্থ-বছর</td>
+                                <td>20-21</td>
+                                <td>বই নং</td>
+                                <td>3</td>
+                                <td>গ্রামঃ</td>
+                                <td>হরিশংকরপুর</td>
+                            </tr>
+
+                            <tr class="row100">
+                                <td>জমাদানের শেষ তারিখ</td>
+                                <td colspan="2">10-10-2020</td>
+                                <td colspan="2">সর্বমোট</td>
+                                <td>2961</td>
+                                <td>টাকা</td>
+                                <td>--</td>
+                                
+                            </tr>
+
+                    
+                        </tbody>
+    
+    
+    
+                    </table>
+
+                </div>
 
         </div>
        
